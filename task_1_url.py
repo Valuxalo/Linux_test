f54a53897561f1423c3762b252f332e173f5e700
Valuxalo <cleo200997@mail.ru>
2021-09-25 19:58:57 +0300

    my final version_

--- task_1_url.py
+++ task_1_url.py
@@ -5,14 +5,12 @@ def url_check(a):
     pattern = r"href=.*?html"
     s_url = []
     res = requests.get(a)
-    st = res.status_code
-    if str(st) == '200':
-        in_a = str(res.content)
-        summ = re.findall(pattern, in_a)
-        for j in range(len(summ)):
-            idx_1 = re.search(pattern, in_a)
-            s_url.append(in_a[idx_1.start() + 6:idx_1.end()])
-            in_a = in_a[idx_1.end() + 1:]
+    in_a = str(res.content)
+    summ = re.findall(pattern, in_a)
+    for j in range(len(summ)):
+        idx_1 = re.search(pattern, in_a)
+        s_url.append(in_a[idx_1.start() + 6:idx_1.end()])
+        in_a = in_a[idx_1.end() + 1:]
     return s_url
 
 flag = 0
@@ -24,7 +22,4 @@ for j in ss:
         flag = 1
         break
 
-if flag == 1:
-    print('Yes')
-else:
-    print('No')
+print('Yes' if flag == 1 else 'No')
